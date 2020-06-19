280c3374d69c19dbb177f048eb90d21d41791134
nlist2 <noah2list@gmail.com>
2020-06-19 15:30:25 -0500

    app push

---
+++ Tip Calculator/Tip Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip Calculator
+//
+//  Created by John List on 6/19/20.
+//  Copyright © 2020 Noah List. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
