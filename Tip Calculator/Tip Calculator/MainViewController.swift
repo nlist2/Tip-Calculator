2b141494155ce4657dec6d7717f1b27663e8c254
Wes Griffin <wesgriffin32@gmail.com>
2020-06-19 15:55:21 -0500

    Beginning of app
    well we started the app

---
+++ Tip Calculator/Tip Calculator/MainViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MainViewController.swift
+//  Tip Calculator
+//
+//  Created by Wes Griffin on 6/19/20.
+//  Copyright © 2020 Noah List. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    @IBOutlet weak var Button10: UIButton!
+    @IBOutlet weak var Button15: UIButton!
+    @IBOutlet weak var Button20: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
