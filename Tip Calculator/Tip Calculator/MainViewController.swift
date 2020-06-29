b8fdb792c8b43cddb29b49e590ae9a5662ac7e6d
Wes Griffin <wesgriffin32@gmail.com>
2020-06-29 15:26:38 -0500

    Messed around with making it work. Did not work :frowning:
    I made a detailed description in the comments.

--- Tip Calculator/Tip Calculator/MainViewController.swift
+++ Tip Calculator/Tip Calculator/MainViewController.swift
@@ -15,13 +15,28 @@ class MainViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var Button20: UIButton!
     @IBOutlet weak var MealCostField: UITextField!
     @IBOutlet weak var TipPercentField: UITextField!
+    @IBOutlet weak var TipAmount: UILabel!
+    @IBOutlet weak var TotalAmount: UILabel!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         var MealCost = MealCostField.text ?? ""
         var TipPercentage = TipPercentField.text ?? ""
+        var TipAmountField = TipAmount.text ?? ""
+        var TotalAmountField = TotalAmount.text ?? ""
+        
+        
         // if meal cost is a string then return "please input number"
         
+        //I tried to make it work, but there were lots of errors about converting from a string to a double and back. This is what I was using \/ but it only worked with mystring and myfloat.
+        
+        let myString = "556"
+        let myFloat = (myString as NSString).doubleValue
+        
+        var ooga = myFloat * myFloat
+        
+        TotalAmount.text = ooga.description
         // to-do
         // configure UITextFields and save input as var
         // solve the math
